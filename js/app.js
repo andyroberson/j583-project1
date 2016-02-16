0446da77710c69c1a4935b997cb213664042b5c7
Andy Roberson <andyroberson2@gmail.com>
2016-02-15 23:56:30 -0500

    trying to get jSON to display

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 var app = angular.module('myApp', []);
 
-app.controller('BaseController', function() {
+app.controller('BaseController', ['$http', function($http) {
 
     this.message = "Ready";
 
-});
+    this.states = [];
+
+    $http.get('js/states.json')
+
+        .success(function(data) {
+          _this.states = data;
+        })
+        .error(function(msg) {
+          console.log("this request failed.\n" + msg);
+        });
+}]);
