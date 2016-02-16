a9f276b289c6b30b65c3ec7d77bdc26701989b63
Andy Roberson <andyroberson2@gmail.com>
2016-02-16 00:20:11 -0500

    corrected and validated JSON still having trouble with output

--- js/app.js
+++ js/app.js
@@ -14,4 +14,13 @@ app.controller('BaseController', ['$http', function($http) {
         .error(function(msg) {
           console.log("this request failed.\n" + msg);
         });
+
 }]);
+
+/* other to do :
+
+map: create for loop that goes through state names and if state name is the same as
+div id, assign the data to that state?
+
+add more data to json file
+*/
