4f17f00b8e8f86dc2a1fbdc9645342492b0b0230
Andy Roberson <andyroberson2@gmail.com>
2016-02-16 00:29:41 -0500

    fixed JSON, appearing in server now

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ app.controller('BaseController', ['$http', function($http) {
 
     this.states = [];
 
+    var _this = this;
+
     $http.get('js/states.json')
 
         .success(function(data) {
@@ -19,8 +21,14 @@ app.controller('BaseController', ['$http', function($http) {
 
 /* other to do :
 
+get it to nicely display all necessary stuff; maybe make a click div?
+
 map: create for loop that goes through state names and if state name is the same as
 div id, assign the data to that state?
 
-add more data to json file
+see https://intridea.github.io/stately/
+
+add more data to json file - images and platform points
+
+create rough design prototype?
 */
